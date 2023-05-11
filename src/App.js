fed2eaa5873ac1874e00ad5ff99344bc927db86d
AJ Dorfman <ajdorfman91@gmail.com>
2023-05-11 16:51:45 -0700

    add code to the newly created repo on github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+function Gameboard() {
+  const [gameState, setGameState] = useState(Array(9).fill(null));
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {gameState[i]}
+      </button>
+    );
+  }
+
+  function handleClick(i) {
+    const newGameState = [...gameState];
+    newGameState[i] = 'X';
+    setGameState(newGameState);
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function Player({ name, mark }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{mark}</p>
+    </div>
+  );
+}
+
+function Game() {
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Gameboard />
+      </div>
+      <div className="game-info">
+        <Player name="Player 1" mark="X" />
+        <Player name="Player 2" mark="O" />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Game />
     </div>
   );
 }
